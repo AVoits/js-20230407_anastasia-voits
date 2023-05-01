b0eba1fc99bb0ff08aa7b4b057d7286a04afb2c6
AVoitsenya <vojtsenyaas@it2g.ru>
2023-05-01 17:49:28 +0300

    Lesson 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,72 @@
 export default class NotificationMessage {
+  static current= null;
 
+  root = document.body;
+  element = null;
+  timerId = null;
+
+  constructor(text = '', options = {}) {
+    this.text = text;
+    this.duration = options.duration;
+    this.type = options.type;
+
+    this.getTemplate();
+  }
+
+  show(root) {
+    if (root) {
+      this.root = root;
+    }
+
+    if (NotificationMessage.current) {
+      this.destroy();
+    }
+
+    this.render();
+  }
+
+
+  getTemplate() {
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML = `
+      <div class="notification ${this.type}" style="--value:${this.duration}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.text}
+          </div>
+        </div>
+      </div>
+    `;
+
+    this.element = wrapper.firstElementChild;
+  }
+
+  render() {
+    this.getTemplate();
+    NotificationMessage.current = this;
+    this.root.append(this.element);
+
+    this.timerId = setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  destroy() {
+    clearTimeout(this.timerId);
+
+    if (NotificationMessage.current) {
+      NotificationMessage.current.remove();
+    }
+
+    this.remove();
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
 }
